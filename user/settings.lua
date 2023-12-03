4546320fbd3b900b614cfc71ca7349724bdff33a
wanlce <55907021+wanlce@users.noreply.github.com>
2023-12-03 16:56:22 +0800

    update settings

---
+++ user/settings.lua
@@ -0,0 +1,15 @@
+-- Please check `lua/core/settings.lua` to view the full list of configurable settings
+local settings = {}
+
+-- Examples
+settings["use_ssh"] = true
+
+settings["colorscheme"] = "catppuccin"
+
+-- Disable the following two plugins
+settings["disabled_plugins"] = {
+	"zbirenbaum/copilot.lua",
+	"zbirenbaum/copilot-cmp",
+}
+
+return settings
